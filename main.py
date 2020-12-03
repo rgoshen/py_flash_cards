95e8d9bd42d7e15b79357c42ffd6b6382d3588e3
Richard Goshen <rick.goshen@gmail.com>
2020-12-03 08:38:23 -0700

    added card flipping

--- main.py
+++ main.py
@@ -1,26 +1,62 @@
 from tkinter import *
+import pandas
+from random import choice, random
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+
+data = pandas.read_csv("./data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+def next_card():
+    """Generates next flash card."""
+    global current_card, flip_timer
+    
+    window.after_cancel(flip_timer)
+    
+    current_card = choice(to_learn)
+    
+    # front of card    
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    
+    flip_timer = window.after(3000, func=flip_card)
+    
+def flip_card():
+    """Flips the flash card from front to back."""
+    
+    # back of card
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+    
 
 # --------------------------- UI SETUP -------------------------------- #
 window = Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 # Flash card
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+card_back_img = PhotoImage(file="./images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Buttons
 x_image = PhotoImage(file="./images/wrong.png")
 check_image = PhotoImage(file="./images/right.png")
-unknown_button = Button(image=x_image, highlightthickness=0)
+unknown_button = Button(image=x_image, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
-known_button = Button(image=check_image, highlightthickness=0)
+known_button = Button(image=check_image, highlightthickness=0, command=next_card)
 known_button.grid(row=1, column=1)
+
+next_card()
+
 window.mainloop()
